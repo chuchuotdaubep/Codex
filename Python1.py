a378ead77f223b312f1e8c696645d59878c40190
Nguyen Phuc Tho <98458593+chuchuotdaubep@users.noreply.github.com>
2022-11-16 18:25:24 +0700

    Add files via upload

---
+++ Python1.py
@@ -0,0 +1,9 @@
+def muabantraicay():
+  name = input("Nhập tên trái cây muốn ăn: ")
+  fruits = ["Cam", "Chanh", "Táo", "Xoài"]
+  foods  = ["Chả", "Mắm", "Xúc xích"]
+  if name in fruits:
+    print("\nChờ xíu ra liền (￣y▽￣)╭ Ohohoho.....")
+  elif name in foods:
+    print("\nĐi chỗ khác mua giùm (oﾟvﾟ)ノ")
+muabantraicay()
