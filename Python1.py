2d4afc948daabe33294e898912499e94b8a91696
Nguyen Phuc Tho <98458593+chuchuotdaubep@users.noreply.github.com>
2022-11-27 00:18:42 +0700

    Delete Python1.py

--- Python1.py
+++
@@ -1,9 +0,0 @@
-def muabantraicay():
-  name = input("Nhập tên trái cây muốn ăn: ")
-  fruits = ["Cam", "Chanh", "Táo", "Xoài"]
-  foods  = ["Chả", "Mắm", "Xúc xích"]
-  if name in fruits:
-    print("\nChờ xíu ra liền (￣y▽￣)╭ Ohohoho.....")
-  elif name in foods:
-    print("\nĐi chỗ khác mua giùm (oﾟvﾟ)ノ")
-muabantraicay()
